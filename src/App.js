cc49629a23d5bf34a9f20c6fddeb18102ac00c57
Vinicius Alvim <vini0191@gmail.com>
2020-06-03 21:07:10 -0300

    Add text and style in carousel component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
+import './App.css';
 
-import Navbar from "./components/Navbar/Navbar.component";
-import Carousel from "./components/Carousel/Carousel.component";
-import About from "./components/About/About.component";
-import Menu from "./components/Menu/Menu.component";
-import Event from "./components/Event/Event.component";
-import Contact from "./components/Contact/Contact.component";
-import Footer from "./components/Footer/Footer.component";
+import Navbar from './components/Navbar/Navbar.component';
+import Carousel from './components/Carousel/Carousel.component';
+import About from './components/About/About.component';
+import Menu from './components/Menu/Menu.component';
+import Event from './components/Event/Event.component';
+import Contact from './components/Contact/Contact.component';
+import Footer from './components/Footer/Footer.component';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Carousel />
-      <Menu />
-      <About />
-      <Event />
-      <Contact />
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+    <Carousel />
+    <Menu />
+    <About />
+    <Event />
+    <Contact />
+    <Footer />
+  </div>
+);
 
 export default App;
