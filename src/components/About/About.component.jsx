16cbb877f2124c300806174872d137cac28e51ef
Vinicius Alvim <vini0191@gmail.com>
2020-06-05 10:44:14 -0300

    Add react-scroll and fix vertical bar in navbar

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Element } from 'react-scroll';
 
 import {
   AboutContainer,
@@ -19,65 +20,66 @@ import about1 from '../../assets/images/about1.png';
 import about2 from '../../assets/images/about2.png';
 
 const About = () => (
-  <AboutContainer>
-    <AboutDiv1>
-      <Sub1>
-        <AboutTitle>
-          <span>Sonho</span>& Dedicação
-        </AboutTitle>
-        <AboutText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
-          fringilla malesuada. Lorem ipsum dolor sit amet,consectetur adipiscing
-          elit. Morbi lobortis enim sed mi fringilla malesuada. Morbi lobortis
-          enim sed mi fringilla malesuada.
-        </AboutText>
-      </Sub1>
-      <Sub2>
-        <AboutImage>
-          <img src={about1} alt="" />
-        </AboutImage>
-      </Sub2>
-    </AboutDiv1>
-    <AboutDiv3>
-      <Sub2>
-        <AboutImage>
-          <img src={about1} alt="" />
-        </AboutImage>
-      </Sub2>
-      <Sub1>
-        <AboutTitle>
-          <span>Sonho</span>& Dedicação
-        </AboutTitle>
-        <AboutText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
-          fringilla malesuada. Lorem ipsum dolor sit amet,consectetur adipiscing
-          elit. Morbi lobortis enim sed mi fringilla malesuada. Morbi lobortis
-          enim sed mi fringilla malesuada.
-        </AboutText>
-      </Sub1>
-    </AboutDiv3>
-    <AboutDiv2>
-      <Sub1>
-        <AboutTitle2>
-          <span>Família</span>& Tradição
-        </AboutTitle2>
-        <AboutText2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
-          fringilla malesuada. Lorem ipsum dolor sit amet,consectetur adipiscing
-          elit. Morbi lobortis enim sed mi fringilla malesuada. Morbi lobortis
-          enim sed mi fringilla malesuada.
-        </AboutText2>
-      </Sub1>
-      <Sub2>
-        <AboutImage2>
-          <img src={about2} alt="" />
-        </AboutImage2>
-      </Sub2>
-    </AboutDiv2>
-    {/* <AboutDiv3>
+  <Element id="sobre" name="sobre">
+    <AboutContainer>
+      <AboutDiv1>
+        <Sub1>
+          <AboutTitle>
+            <span>Sonho</span>& Dedicação
+          </AboutTitle>
+          <AboutText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
+            fringilla malesuada. Lorem ipsum dolor sit amet,consectetur
+            adipiscing elit. Morbi lobortis enim sed mi fringilla malesuada.
+            Morbi lobortis enim sed mi fringilla malesuada.
+          </AboutText>
+        </Sub1>
+        <Sub2>
+          <AboutImage>
+            <img src={about1} alt="" />
+          </AboutImage>
+        </Sub2>
+      </AboutDiv1>
+      <AboutDiv3>
+        <Sub2>
+          <AboutImage>
+            <img src={about1} alt="" />
+          </AboutImage>
+        </Sub2>
+        <Sub1>
+          <AboutTitle>
+            <span>Sonho</span>& Dedicação
+          </AboutTitle>
+          <AboutText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
+            fringilla malesuada. Lorem ipsum dolor sit amet,consectetur
+            adipiscing elit. Morbi lobortis enim sed mi fringilla malesuada.
+            Morbi lobortis enim sed mi fringilla malesuada.
+          </AboutText>
+        </Sub1>
+      </AboutDiv3>
+      <AboutDiv2>
+        <Sub1>
+          <AboutTitle2>
+            <span>Família</span>& Tradição
+          </AboutTitle2>
+          <AboutText2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+            lobortis enim sed mi fringilla malesuada. Morbi lobortis enim sed mi
+            fringilla malesuada. Lorem ipsum dolor sit amet,consectetur
+            adipiscing elit. Morbi lobortis enim sed mi fringilla malesuada.
+            Morbi lobortis enim sed mi fringilla malesuada.
+          </AboutText2>
+        </Sub1>
+        <Sub2>
+          <AboutImage2>
+            <img src={about2} alt="" />
+          </AboutImage2>
+        </Sub2>
+      </AboutDiv2>
+      {/* <AboutDiv3>
       <Sub1>
         <AboutTitle2>
           <span>Família</span>& Tradição
@@ -96,7 +98,8 @@ const About = () => (
         </AboutImage2>
       </Sub2>
     </AboutDiv3> */}
-  </AboutContainer>
+    </AboutContainer>
+  </Element>
 );
 
 export default About;
--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Navbar, Nav } from './Navbar.style';
-import { NavbarContainer } from './Navbar.style';
+import { NavbarContainer, Navbar, Nav } from './Navbar.style';
+import Scroll from 'react-scroll';
+
 import LogoMobile from '../../assets/images/logo_mob.png';
 import Logo from '../../assets/images/logo.png';
 import FacebookMobileIcon from '../../assets/images/facebook_mob.png';
@@ -9,64 +10,74 @@ import InstagramMobileIcon from '../../assets/images/instagram_mob.png';
 import FacebookIcon from '../../assets/images/facebook.png';
 import InstagramIcon from '../../assets/images/instagram.png';
 
-const NavbarDefault = ({ showCarousel, setShowCarousel }) => (
-  <NavbarContainer>
-    <Navbar expand="md">
-      <Navbar.Brand>
-        <img className="logo-mobile" src={LogoMobile} alt="" />
-        <img className="logo" src={Logo} alt="" />
-      </Navbar.Brand>
-      <Navbar.Toggle
-        aria-controls="basic-navbar-nav"
-        onClick={() => setShowCarousel(!showCarousel)}
-      />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Item className="first-nav-item">
-            <button>
-              Home<span className="bar">|</span>
-            </button>
-          </Nav.Item>
-          <Nav.Item>
-            <button>
-              Cardápio<span className="bar">|</span>
-            </button>
-          </Nav.Item>
-          <Nav.Item>
-            <button>
-              Sobre<span className="bar">|</span>
-            </button>
-          </Nav.Item>
-          <Nav.Item>
-            <button>
-              Equipe<span className="bar">|</span>
-            </button>
-          </Nav.Item>
-          <Nav.Item className="last-nav-item">
-            <button>Contato</button>
-          </Nav.Item>
-          <Nav.Item className="icons-container">
-            <a
-              href="https://www.facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className="icon-mobile" src={FacebookMobileIcon} alt="" />
-              <img className="icon fb" src={FacebookIcon} alt="" />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img className="icon-mobile" src={InstagramMobileIcon} alt="" />
-              <img className="icon inst" src={InstagramIcon} alt="" />
-            </a>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </NavbarContainer>
-);
+const NavbarDefault = ({ showCarousel, setShowCarousel }) => {
+  const ScrollLink = Scroll.Link;
+
+  return (
+    <NavbarContainer>
+      <Navbar expand="md">
+        <Navbar.Brand>
+          <img className="logo-mobile" src={LogoMobile} alt="" />
+          <img className="logo" src={Logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setShowCarousel(!showCarousel)}
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <ScrollLink to="home" spy={true} smooth={true} duration={500}>
+              <Nav.Item className="first-nav-item">
+                <button>Home</button>
+                <span className="bar">|</span>
+              </Nav.Item>
+            </ScrollLink>
+            <ScrollLink to="cardapio" spy={true} smooth={true} duration={1000}>
+              <Nav.Item>
+                <button>Cardápio</button>
+                <span className="bar">|</span>
+              </Nav.Item>
+            </ScrollLink>
+            <ScrollLink to="sobre" spy={true} smooth={true} duration={1000}>
+              <Nav.Item>
+                <button>Sobre</button>
+                <span className="bar">|</span>
+              </Nav.Item>
+            </ScrollLink>
+            <ScrollLink to="equipe" spy={true} smooth={true} duration={1000}>
+              <Nav.Item>
+                <button>Equipe</button>
+                <span className="bar">|</span>
+              </Nav.Item>
+            </ScrollLink>
+            <ScrollLink to="contato" spy={true} smooth={true} duration={1000}>
+              <Nav.Item className="last-nav-item">
+                <button>Contato</button>
+              </Nav.Item>
+            </ScrollLink>
+            <Nav.Item className="icons-container">
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="icon-mobile" src={FacebookMobileIcon} alt="" />
+                <img className="icon fb" src={FacebookIcon} alt="" />
+              </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="icon-mobile" src={InstagramMobileIcon} alt="" />
+                <img className="icon inst" src={InstagramIcon} alt="" />
+              </a>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </NavbarContainer>
+  );
+};
 
 export default NavbarDefault;
